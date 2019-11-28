2deb26af03407a636d8f80a85c29ca55ce5602d3
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2019-11-28 23:20:33 +0300

    Added masked input

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+import '../project/modules/forms/__input/_masked/jquery.maskedinput.js'
 
-
-$(function(){                   // Start when document ready
+$(document).ready(function($){                 // Start when document ready
     $('.star_block#star-rating').rating(); // Call the rating plugin
-}); 
+    $('#masked').mask("99/99/9999");
+});
