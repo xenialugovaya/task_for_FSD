251bb5400b89da50fc9f35e73988b8edaaff4950
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2019-12-04 00:49:26 +0300

    Added guests dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,9 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
+import '../project/modules/forms/__input/form__input'
 
-
-$(document).ready(function($){                 // Start when document ready
-    $('.star_block#star-rating').rating(); // Call the rating plugin
-    $.mask.definitions['y'] = '[12]';
-    $.mask.definitions['m'] = '[01]';
-    $.mask.definitions['d'] = '[0-3]';
-    $('#masked').mask("d9.m9.y999");
-
-    $('.bedroom .iqdropdown').multipleDropdown({
-        selectionText: ['спальня', 'кровать', 'ванная комната'],
-        textPlural: ['спальни', 'кровати', 'ванные комнаты'],
-        textDefault: ['спален', 'кроватей', 'ванных комнат'],
-       
-    });
+$(document).ready(function($){                 
+    $('.star_block#star-rating').rating(); 
   
 });
--- src/project/modules/forms/__input/form__input.js
+++ src/project/modules/forms/__input/form__input.js
@@ -1,1 +1,27 @@
-import './_masked/masked.js'
+import 'item-quantity-dropdown/src/index.js'
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
+import './_dropdown/_multiple-dropdown'
+import './_masked/jquery.maskedinput'
+
+$(document).ready(function($){                 
+
+
+    $('.bedroom .iqdropdown').multipleDropdown({
+        selectionText: ['спальня', 'кровать', 'ванная комната'],
+        textPlural: ['спальни', 'кровати', 'ванные комнаты'],
+        textDefault: ['спален', 'кроватей', 'ванных комнат'],
+       
+    });
+    $('.guests .iqdropdown').iqDropdown({
+        selectionText: 'гость',
+        textPlural: 'гостя',
+        textDefault: 'Cколько гостей',
+       
+    });
+
+    $.mask.definitions['y'] = '[12]';
+    $.mask.definitions['m'] = '[01]';
+    $.mask.definitions['d'] = '[0-3]';
+    $('#masked').mask("d9.m9.y999");
+  
+});
