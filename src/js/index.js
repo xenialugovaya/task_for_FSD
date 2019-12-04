4db41965a83ede9bd52caec5a2802aa8957bd354
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2019-12-04 21:21:00 +0300

    Added expandable checkbox

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
 import '../project/modules/forms/__input/form__input'
+import '../project/modules/checkbox-group/checkbox-expandable/checkbox-expandable'
+
 
 $(document).ready(function($){                 
     $('.star_block#star-rating').rating(); 
