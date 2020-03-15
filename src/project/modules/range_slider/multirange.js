4638fba4e57f11b4e11cabcfe9f265da5ea9f635
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-03-15 16:08:41 +0300

    modified label style of range slider

--- src/project/modules/range_slider/multirange.js
+++ src/project/modules/range_slider/multirange.js
@@ -4,10 +4,12 @@ $( function() {
       min: 0,
       max: 16000,
       values: [ 5000, 10000 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val(ui.values[ 0 ] + "Р - " + ui.values[ 1 ] + "Р");
+      slide: function( event, ui ) {  
+        $( "#amount" ).val(ui.values[ 0 ].toLocaleString() + "\u20BD - " + ui.values[ 1 ].toLocaleString() + "\u20BD");
       }
     });
-    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 )+  "Р  - "  +
-    $( "#slider-range" ).slider( "values", 1 ) + "Р" );
+    
+    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ).toLocaleString() +  "\u20BD  - "  +
+    $( "#slider-range" ).slider( "values", 1 ).toLocaleString() + "\u20BD" );
   } );
+
