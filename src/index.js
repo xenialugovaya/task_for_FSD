806b284794afdf61c86268485cd38f3cf09bcab5
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-03-15 18:46:04 +0300

    modified calendar for login form to work with 2 inputs

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import 'webpack-jquery-ui/css'
 import './project/modules/ratings/rating'
 import './project/modules/donut-chart/donut-chart'
 import './project/modules/range_slider/multirange'
-//import 'air-datepicker/dist/css/datepicker.min.css'
 import 'air-datepicker/dist/js/datepicker.js'
-import './project/modules/forms/__input/_small/form__input-small' //надо будет пересмотреть и отключить
 import './js/'
-import './project/modules/forms/__calendar-block/form__calendar-block' //не создан
+import './project/modules/forms/__calendar-block/form__calendar-block' 
 import 'jquery.mmenu/dist/jquery.mmenu.all.css'
 import 'jquery.mmenu/dist/jquery.mmenu.all.js'
 import 'mburger-css/dist/mburger.js'
