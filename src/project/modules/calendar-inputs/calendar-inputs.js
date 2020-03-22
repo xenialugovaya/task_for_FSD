861fe09ab376b486e715d63992c715b85c8be997
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-03-22 20:27:17 +0300

    refactored calendar block with 2 inputs

---
+++ src/project/modules/calendar-inputs/calendar-inputs.js
@@ -0,0 +1,66 @@
+import './calendar-inputs.scss';
+import $ from 'jquery';
+
+import 'air-datepicker/dist/js/datepicker.js'
+
+class CalendarInputs {
+  constructor($element) {
+    this.$elem = $element;
+    this.showCalendar();
+    this.setDatesToInputs();
+    this.hideCalendar();
+  }
+
+  showCalendar() {
+    this.$elem.find('.small-input', this.$elem).on('click', () => {
+      this.$elem.find('.calendar-dropdown', this.$elem).show();
+    });
+  }
+
+  hideCalendar() {
+    this.$elem.find('span[data-action="add"]', this.$elem).on('click', () => {
+      this.$elem.find('.calendar-dropdown', this.$elem).hide();
+    });
+  }
+
+  setDatesToInputs() {
+    let $datepicker = this.getCalendarData();
+    this.$elem.find('.datepicker-here', this.$elem).on("click", () => {
+      this.clearInputs();
+      if ($datepicker.selectedDates[0]) {
+        $('.small-input', this.$elem).eq(0).val(this.formatDate($datepicker.selectedDates[0]));
+      }
+      if ($datepicker.selectedDates[1]) {
+        $('.small-input', this.$elem).eq(1).val(this.formatDate($datepicker.selectedDates[1]));
+      }
+    });
+  }
+
+  clearInputs() {
+    this.$elem.find('.small-input', this.$elem).val(' ');
+  }
+
+  getCalendarData() {
+    let $datepicker = this.$elem.find('.datepicker-here', this.$elem).datepicker().data('datepicker');
+    return $datepicker;
+  }
+
+  formatDate(date) {
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+    let yy = date.getFullYear();
+    return dd + '.' + mm + '.' + yy;
+  }
+}
+
+export default function renderComponent() {
+  $(() => {
+    $('.js-calendar-inputs').each((index, node) => {
+      new CalendarInputs($(node));
+    });
+  });
+}
+
+renderComponent();
