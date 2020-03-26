30b0c1e825405dfa7f8e13e1fe5c879c8d5b127c
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-03-26 21:40:46 +0300

    refactored donut chart

--- src/project/modules/donut-chart/donut-chart.js
+++ src/project/modules/donut-chart/donut-chart.js
@@ -1,17 +1,31 @@
 import './donut-chart.scss';
 
-$(document).ready(function($) {
+class DonutChart {
+  constructor(element) {
+    this.$elem = element;
+    this.render();
+  }
+  render() {
+    this.$elem.donutChart({
+      chartOptions: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
+      amountAbs: [520, 260, 260],
+      gradients: [
+        ['#FFE39C', '#FFBA9C'],
+        ['#6FCF97', '#66D2EA'],
+        ['#BC9CFF', '#8BA4F9'],
+        ['#919191', '#3D4975'],
+        ['#BC9CFF', '#8BA4F9'],
+      ],
+    });
+  }
+}
 
-  $('.donut-chart').donutChart({
-    chartOptions: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
-    amountAbs: [520, 260, 260],
-    gradients: [
-      ['#FFE39C', '#FFBA9C'],
-      ['#6FCF97', '#66D2EA'],
-      ['#BC9CFF', '#8BA4F9'],
-      ['#919191', '#3D4975'],
-      ['#BC9CFF', '#8BA4F9'],
-    ],
+export default function renderComponent() {
+  $(() => {
+    $('.js-donut-chart').each((index, node) => {
+      new DonutChart($(node));
+    });
   });
+}
 
-});
+renderComponent();
