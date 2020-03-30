76f3a9b512cda93e8e9e5651b45b8dfaeee47f1f
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-03-30 16:09:42 +0300

    added favicon

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
 }
 
-requireAll(require.context('./project/modules', true, /\.js$/)); // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-requireAll(require.context('./project/pages', true, /\.js$/));
-requireAll(require.context('./project/layout', true, /\.js$/));
+requireAll(require.context('../src/', true, /\.js$/)); // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
+//requireAll(require.context('./project/pages', true, /\.js$/));
+//requireAll(require.context('./project/layout', true, /\.js$/));
