a401c67da35483bd608f3571e67a6e2b175a2713
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-04-03 19:31:41 +0300

    #6 finished with adjusting to BEM

--- src/project/modules/range-slider/range-slider.js
+++ src/project/modules/range-slider/range-slider.js
@@ -16,13 +16,13 @@ class RangeSlider {
       max: 16000,
       values: [5000, 10000],
       slide: (event, ui) => {
-        this.$elem.find('.selected-amount').val(`${ui.values[0].toLocaleString()}\u20BD - ${ui.values[1].toLocaleString()}\u20BD`);
+        this.$elem.find('.range-slider__selected-amount').val(`${ui.values[0].toLocaleString()}\u20BD - ${ui.values[1].toLocaleString()}\u20BD`);
       },
     });
   }
 
   setValuesToInput() {
-    this.$elem.find('.selected-amount').val(`${this.$elem.find('.js-range-slider__body').slider('values', 0).toLocaleString()}\u20BD - ${
+    this.$elem.find('.range-slider__selected-amount').val(`${this.$elem.find('.js-range-slider__body').slider('values', 0).toLocaleString()}\u20BD - ${
       this.$elem.find('.js-range-slider__body').slider('values', 1).toLocaleString()}\u20BD`);
   }
 }
