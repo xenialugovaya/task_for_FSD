9f05f9b6c50cca2455cc378609b9f7b1a746879f
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-04-04 22:09:04 +0300

    #3 refactored calendar

--- src/project/modules/calendar/calendar.js
+++ src/project/modules/calendar/calendar.js
@@ -1,2 +1,37 @@
 import 'air-datepicker/dist/js/datepicker';
-import './calendar.scss';
+import $ from 'jquery';
+
+
+class Calendar {
+  constructor(element) {
+    this.$elem = element;
+    this.addApplyButton();
+  }
+
+  addApplyButton() {
+    const applyButton = document.createElement('span');
+    applyButton.classList.add('datepicker--button');
+    applyButton.setAttribute('data-action', 'add');
+    applyButton.innerText = 'Применить';
+    const $buttonsBlock = this.$elem.find('.datepicker--buttons');
+    $buttonsBlock.append(applyButton);
+    $(applyButton).on('click', this.handleApplyEvent.bind(this));
+  }
+
+  handleApplyEvent() {
+    const parent = this.$elem.parent();
+    if ($(parent).hasClass('calendar-inputs__calendar-dropdown')) {
+      parent.hide();
+    }
+  }
+}
+
+export default function renderComponent() {
+  $(() => {
+    $('.js-calendar').each((index, node) => {
+      const calendar = new Calendar($(node));
+      return calendar;
+    });
+  });
+}
+renderComponent();
--- src/project/modules/input-date-dropdown/input-date-dropdown.js
+++ src/project/modules/input-date-dropdown/input-date-dropdown.js
@@ -8,13 +8,31 @@ class DropCalendar {
   }
 
   render() {
-    this.$elem.datepicker({
+    this.datepicker = this.$elem.datepicker({
       range: true,
       clearButton: true,
-      addButton: true,
       multipleDatesSeparator: ' - ',
       dateFormat: 'd M',
-    });
+      language: {
+        monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+      },
+    }).data('datepicker');
+    const $buttonsBlock = $(this.datepicker.$datepicker).find('.datepicker--buttons');
+    this.addApplyButton($buttonsBlock);
+    return this.datepicker;
+  }
+
+  addApplyButton(parent) {
+    const applyButton = document.createElement('span');
+    applyButton.classList.add('datepicker--button');
+    applyButton.setAttribute('data-action', 'add');
+    applyButton.innerText = 'Применить';
+    parent.append(applyButton);
+    $(applyButton).on('click', this.handleApplyEvent.bind(this));
+  }
+
+  handleApplyEvent() {
+    this.$elem.find('.datepicker').hide();
   }
 }
 
