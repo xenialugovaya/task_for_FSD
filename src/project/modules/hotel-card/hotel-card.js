8fddb6944be0c31b6ddf94b3d7b71ace047698a7
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-04-11 14:41:37 +0300

    #7 added hover style to hotel card

--- src/project/modules/hotel-card/hotel-card.js
+++ src/project/modules/hotel-card/hotel-card.js
@@ -5,21 +5,37 @@ class ImageSlider {
   constructor(element) {
     this.$elem = element;
     this.render();
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.$elem.find('.lSPrev').on('mouseenter', this.addHoverState.bind(this));
+    this.$elem.find('.lSNext').on('mouseenter', this.addHoverState.bind(this));
+    this.$elem.find('.lSSlideWrapper').on('mouseenter', this.addHoverState.bind(this));
+    this.$elem.find('.lSSlideWrapper').on('mouseout', this.addDefaultState.bind(this));
   }
 
   render() {
-    this.$elem.lightSlider({
+    this.$elem.find('.hotel-card__images-list').lightSlider({
       item: 1,
       slideMove: 1,
       loop: true,
       controls: true,
     });
   }
+
+  addHoverState() {
+    this.$elem.find('.hotel-card__slider').addClass('hotel-card__slider_with-controls');
+  }
+
+  addDefaultState() {
+    this.$elem.find('.hotel-card__slider').removeClass('hotel-card__slider_with-controls');
+  }
 }
 
 function renderElement() {
   $(() => {
-    $('.js-hotel-images-list').each((index, node) => {
+    $('.js-hotel-card').each((index, node) => {
       const imageSlider = new ImageSlider($(node));
       return imageSlider;
     });
