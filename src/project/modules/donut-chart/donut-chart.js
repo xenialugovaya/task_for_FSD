4b5163be1faece78bb8d805daa2e86dc7f44a382
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-04-18 22:00:30 +0300

    #3 refactored donut chart

--- src/project/modules/donut-chart/donut-chart.js
+++ src/project/modules/donut-chart/donut-chart.js
@@ -1,4 +1,23 @@
-import DonutChart from './DonutChart';
+class DonutChart {
+  constructor(element) {
+    this.$elem = element;
+    this.render();
+  }
+
+  render() {
+    this.$elem.donutChart({
+      chartOptions: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
+      amountAbs: [520, 260, 260],
+      gradients: [
+        ['#FFE39C', '#FFBA9C'],
+        ['#6FCF97', '#66D2EA'],
+        ['#BC9CFF', '#8BA4F9'],
+        ['#919191', '#3D4975'],
+        ['#BC9CFF', '#8BA4F9'],
+      ],
+    });
+  }
+}
 
 function renderComponent() {
   $(() => {
