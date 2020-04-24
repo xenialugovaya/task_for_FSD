26e2bd20d93f1e30bdcb4aa5d97add9fac95e214
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-04-25 00:29:57 +0300

    #6 unified names of mixins and block, refactored input-dropdown

--- src/project/modules/input-dropdown/input-dropdown.js
+++ src/project/modules/input-dropdown/input-dropdown.js
@@ -3,11 +3,11 @@ import DropdownSingle from './DropdownSingle';
 
 function renderComponent() {
   $(() => {
-    $('.js-input_dropdown-single').each((index, node) => {
+    $('.js-input-dropdown_single').each((index, node) => {
       const dropdownSingle = new DropdownSingle($(node));
       return dropdownSingle;
     });
-    $('.js-input_dropdown-multiple').each((index, node) => {
+    $('.js-input-dropdown_multiple').each((index, node) => {
       const dropdownMultiple = new DropdownMultiple($(node));
       return dropdownMultiple;
     });
